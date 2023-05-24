8100f7fe41bb81ec55dbb358d9f6984bcb1ac703
lucas <lsantana1234@hotmail.com>
2023-05-24 05:26:53 -0300

    operadores

---
+++ javaScript/operadores.js
@@ -0,0 +1,24 @@
+var n = 3
+// n = n - 10
+ n *= 7
+console.log(n)
+
+var x  = 10
+x--
+
+b = 20
+c = 60
+a = 5
+console.log(x)
+console.log(n > x || b !== c)
+// verificar se b é maior que b e b é divisivel por 2
+console.log(a > b && b % 2 == 0)
+
+
+// verificar se a é maior que b e b maior que 5
+console.log(b <= b  || b / 2 ==2)
+
+// verificar se b e par ou impar usando ternario
+resultado = b % 2 == 0 ? "Par" : "Impar"
+
+console.log(resultado)
